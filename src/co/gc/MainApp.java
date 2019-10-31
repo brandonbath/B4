9a3baabef582c1e84d71fbad239b898e2ce21923
brandonbath <brandon_bath@yahoo.com>
2019-10-31 10:26:22 -0400

    initial commit

---
+++ src/co/gc/MainApp.java
@@ -0,0 +1,12 @@
+package co.gc;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		
+		System.out.println("Hello, my name is The Real Antonella");
+
+	}
+
+}
